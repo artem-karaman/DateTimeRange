fb0ba6661c421c4723e1e876740a2f31fcb0b86c
Artem Karaman <AKaraman@luxoft.com>
2018-09-20 16:52:29 +0300

    updated merge extension method

--- DateTimeRange.Core/DateTimeRange/DateTimeRangeExtensions.cs
+++ DateTimeRange.Core/DateTimeRange/DateTimeRangeExtensions.cs
@@ -10,7 +10,34 @@ namespace DateTimeRangeCore.DateTimeRange
 	{
 		public static IEnumerable<DateTimeRange> Merge(this IEnumerable<DateTimeRange> range)
 		{
-			return null;
+			var tmp = new DateTimeRange();
+
+			List<DateTimeRange> ranges = range.ToList();
+			List<DateTimeRange> result = new List<DateTimeRange>();
+			int next = 0;
+
+			for (int i = 0; i < ranges.Count(); i++)
+			{
+				if (i < ranges.Count() - 1)
+				{
+					next ++;
+
+					if (ranges[i].Begin == ranges[next].Begin)
+					{
+						result.Add(new DateTimeRange(ranges[i].Begin, ranges[i].Duration + ranges[next].Duration));
+					}
+					else if (ranges[i].End == ranges[next].Begin)
+					{
+						result.Add(new DateTimeRange(ranges[i].Begin, ranges[i].Duration + ranges[next].Duration));
+					}
+					else
+					{
+						result.Add(new DateTimeRange(ranges[i].Begin, ranges[i].Duration));
+					}
+				}
+			}
+
+			return result;
 		}
 	}
 }
